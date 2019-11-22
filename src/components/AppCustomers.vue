a20536c15de4e863cba1f2d54c711c48b13973e7
Sabolc Keringer <keringersz@gmail.com>
2019-11-22 19:39:33 +0100

    Zadatak 1: Basic layout created for Customers and Products pages

---
+++ src/components/AppCustomers.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h3>Customers page</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
+import VueRouter from "vue-router";
 import App from './App.vue'
+import AppProducts from './components/AppProducts.vue'
+import AppCustomers from './components/AppCustomers.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+const routes = [
+  { name: "index", path: "/", redirect: "/products" },
+  { name: "customers", path: "/customers", component: AppCustomers },
+  { name: "products", path: "/products", component: AppProducts }
+  
+]
+
+const router = new VueRouter({
+  routes
+});
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
+
