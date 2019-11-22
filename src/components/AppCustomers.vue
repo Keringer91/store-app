26d562ebdf0320e672fa0da5748af120941d9134
Sabolc Keringer <keringersz@gmail.com>
2019-11-22 20:43:20 +0100

    Zadatak 3: Add new customer feature added

--- src/components/AppCustomers.vue
+++ src/components/AppCustomers.vue
@@ -8,6 +8,25 @@
             <button @click="removeCustomer(customer)">Remove customer</button>
         </li>
     </ul>
+
+    <form @submit.prevent>
+        <table>
+        <tr>
+          <td><label>First name:</label></td>
+          <td><input v-model="customer.firstName"  type="text" id="firstName"></td>
+        </tr>
+        <tr>
+          <td><label>Last name:</label></td>
+          <td><input v-model="customer.lastName" type="text" id="lastName"></td>
+        </tr>
+        <tr>
+          <td><label>Email adress:</label></td>
+          <td><input v-model="customer.email" type="text" id="email"></td>
+        </tr>
+      </table>  
+      <button @click="addNewCustomer" type="submit">Submit</button>
+    </form>
+
   </div>
 </template>
 
@@ -16,13 +35,20 @@ import { customerService } from '../services/CustomerService'
 export default {
   data () {
     return {
-      customers: customerService.list()
+      customers: customerService.list(),
+      customer: {},
     }
   },
 
   methods: {
       removeCustomer(customer) {
           customerService.remove(customer);
+      },
+      addNewCustomer(){
+        customerService.add({ 
+            fullName: this.customer.firstName+' '+this.customer.lastName,
+            email: this.customer.email
+         });
       }
   }
 }
