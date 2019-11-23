f167d7c380d11a7cdddc308460fb129ee45fc392
Sabolc Keringer <keringersz@gmail.com>
2019-11-23 16:46:35 +0100

    Zadatak 7: Product increment/decrement featurea added

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -10,7 +10,9 @@
         <li>
             ID: {{ product.id}}<br>
             Title: {{ product.name }}<br>
-            Quantity: {{ product.quantity }}<br>            
+            Quantity: {{ product.quantity }}<br>   
+            <button @click="incrementQuantity(product)"> + </button>         
+            <button @click="decrementQuantity(product)"> - </button>         
         </li>
     </ul>
   </div>
@@ -19,20 +21,29 @@
 <script>
 import { productService } from '../services/ProductService'
 export default {
-  data () {
-    return {
-        search: '',
-        products: [],
-    }
-   },
+    data () {
+        return {
+            search: '',
+            products: [],
+        }
+    },
+    methods: {
+        incrementQuantity(product) {
+            productService.increment(product)
+        },
+        decrementQuantity(product) {
+            productService.decrement(product)
+        }
+    },
 
-   created() {
-     this.products = productService.list()
-   },
-   computed: {
-    filteredList() {
-      return this.products.filter(product => {
-        return product.name.toLowerCase().includes(this.search.toLowerCase())
+    created() {
+        this.products = productService.list()
+    },
+
+    computed: {
+        filteredList() {
+            return this.products.filter(product => {
+            return product.name.toLowerCase().includes(this.search.toLowerCase())
       })
     }
   }
