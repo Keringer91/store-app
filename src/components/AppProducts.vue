69bdfa31dd8981cceaedf149bfd6f5e82ef26d06
Sabolc Keringer <keringersz@gmail.com>
2019-11-23 15:44:58 +0100

    Zadatak 5: All products listed

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,16 +1,29 @@
 <template>
   <div>
-      <h3>Products page</h3>
+      <h3>Products page</h3><br>
+      <h4>All products: </h4>      
+      <ul v-for="(product,index) in products" :key="index">
+        <li>
+            ID: {{ product.id}}<br>
+            Title: {{ product.name }}<br>
+            Quantity: {{ product.quantity }}<br>            
+        </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { productService } from '../services/ProductService'
 export default {
   data () {
     return {
-      
+      products: []
     }
-  }
+   },
+
+   created() {
+     this.products = productService.list()
+   },
 }
 </script>
 
