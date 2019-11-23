3eda1d450790135065908bcbcde140be0bf836b4
Sabolc Keringer <keringersz@gmail.com>
2019-11-23 16:36:33 +0100

    Zadatak 6: Search product feature added

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-      <h3>Products page</h3><br>
-      <h4>All products: </h4>      
-      <ul v-for="(product,index) in products" :key="index">
+    <h3>Products page</h3><br>
+    <h4>All products: </h4>
+
+    <label>Search: </label>
+    <input type="text" v-model="search" placeholder="Search title.."/>
+
+    <ul v-for="(product, index) in filteredList" :key="index">
         <li>
             ID: {{ product.id}}<br>
             Title: {{ product.name }}<br>
@@ -17,13 +21,21 @@ import { productService } from '../services/ProductService'
 export default {
   data () {
     return {
-      products: []
+        search: '',
+        products: [],
     }
    },
 
    created() {
      this.products = productService.list()
    },
+   computed: {
+    filteredList() {
+      return this.products.filter(product => {
+        return product.name.toLowerCase().includes(this.search.toLowerCase())
+      })
+    }
+  }
 }
 </script>
 
